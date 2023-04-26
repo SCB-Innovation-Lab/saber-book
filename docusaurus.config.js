142ee0f955663972a9ad2c4039b5470c81236227
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-04-26 12:03:19 +0700

    update url config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://scb-innovation-lab.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/saber-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
